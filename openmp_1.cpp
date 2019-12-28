4db251a823ae6ce6de6bcad74d5669cb1832cbd3
JustMarie <marie.mingazova@mail.ru>
2019-12-28 23:29:20 +0300

    OpenMP_1 with threads "debuging"

--- openmp_1.cpp
+++ openmp_1.cpp
@@ -1,55 +1,87 @@
+
+#include "stdafx.h"
 #include <iostream>
 #include <stdlib.h>
 #include <omp.h>
 #include <ctime>
+#include <algorithm>
+#include  <iomanip>
+
 using namespace std;
 
 const unsigned int DIM1 = 3;  // rows number
 const unsigned int DIM2 = 5;  //columns number
 
-
-
 float min_max_element(float mtr[DIM1][DIM2])
 {
-    int num_threads = DIM1 * DIM1;
-    omp_set_num_threads(num_threads);
-    float mins[DIM1];
+	float mins[DIM1];
 
-    #pragma omp parallel for
-    for (int i=0; i<DIM1; i++)
-        mins[i] = mtr[i][0];
+#pragma omp parallel for shared(mins, mtr)
+	for (int i = 0; i < DIM1; i++)
+		mins[i] = mtr[i][0];
 
-    #pragma omp parallel for
-    for (int i=0; i<DIM1; i++) {
-        #pragma omp parallel for reduction(min: minimums)
-        for (int j = 0; j < DIM2; j++) {
-            mins[i] = min(mins[i], mtr[i][j]);
-        }
+#pragma omp parallel for
+	for (int i = 0; i < DIM1; i++) {
+		for (int j = 0; j < DIM2; j++) {
+#pragma omp critical
+			{	if (mtr[i][j] < mins[i])
+					mins[i] = mtr[i][j];
+				int myID = omp_get_thread_num();
+				int threads = omp_get_num_threads();
+				std::cout << "Num of thread in inner min-loop is " << myID << " from " << threads << endl;
+			}
+		}
+		int myID = omp_get_thread_num();
+		int threads = omp_get_num_threads();
+		std::cout << "Num of thread in outer min-loop is " << myID << " from " << threads << endl;
 
-    }
+	}
 
-    float min_max_value = mins[0];
+	float max_of_mins = mins[0];
 
-    #pragma omp parallel for reduction(min: minimums)
-    for (int i=0; i<DIM1; i++) {
-        min_max_value = max(min_max_value, mins[i]);
-    }
+#pragma omp parallel for shared(mins, max_of_mins) 
+	for (int i = 0; i < DIM1; i++) {
+#pragma omp critical
+		{
+		if (mins[i] > max_of_mins) {
+			max_of_mins = mins[i];
+		}
+		int myID = omp_get_thread_num();
+		int threads = omp_get_num_threads();
+		std::cout << "Num of thread in outer min-loop is " << myID << " from " << threads << endl;
+		}
+	}
+	
 
-    return min_max_value;
+	return max_of_mins;
+}
+
+void print_matrix(float a[DIM1][DIM2]) {
+
+	for (int r = 0; r < DIM1; ++r) {
+		for (int c = 0; c < DIM2; ++c)
+			cout << setw(5) << a[r][c] << " ";
+		cout << endl;
+	}
 }
 
 int main() {
-    float matrix[DIM1][DIM2];
+	float matrix[DIM1][DIM2];
 
-    std::srand(unsigned(std::time(0)));
-    for (int i=0; i<DIM1; i++) {
-        for (int j = 0; j < DIM2; j++) {
-            matrix[i][j] = rand();
-        }
+	std::srand(unsigned(std::time(0)));
+	//srand(1);
+	for (int i = 0; i < DIM1; i++) {
+		for (int j = 0; j < DIM2; j++) {
+			matrix[i][j] = rand()%100;
+		}
 
-    }
-   min_max = min_max_element(matrix);
+	}
 
-    return 0;
+
+	print_matrix(matrix);
+	float min_max = min_max_element(matrix);
+
+	cout << endl << "Min_max_element is " << min_max << endl;
+	return 0;
 }
 
