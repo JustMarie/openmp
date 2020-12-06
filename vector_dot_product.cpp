40dd380a991b3ddfee448dd3245acd5a44ec358e
MarieMin <marie.mingazova@mail.ru>
2020-12-06 16:44:35 +0300

    Update vector_dot_product.cpp

--- vector_dot_product.cpp
+++ vector_dot_product.cpp
@@ -1,45 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include <cmath>
-#include <chrono>
+#include <iostream>
+#include <ctime>
 #include <algorithm> 
 #include <vector> 
 
 using namespace std;
-using namespace std::chrono;
 
-const unsigned int N = 10000000;
-const unsigned NUM_THREADS = 4;
-
-
-int main()
+void dotProduct(std::vector<int> a, std::vector<int> b, int num_threads, int N)
 {
-	vector<int> values1(N);
-	vector<int> values2(N);
+	double sum_paral_red = 0.0;
+	double sum_paral_atomic = 0.0;
+	double sum_seq = 0.0;
+	int i;
 
-	int i, sum = 0.0;
+	double startReductionOMP;  
+	double endReductionOMP;
+	double startAtomic;
+	double endAtomic;
+	double startConseq;   
+	double endConseq;
 
-	auto f = []() -> int { return rand() % 10000; };
-	generate(values1.begin(), values1.end(), f);
-	generate(values2.begin(), values2.end(), f);
+	omp_set_dynamic(0);
 
-	omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel num_threads(num_threads)
+	if (omp_get_thread_num() == 0)
+	{
+		printf("\n\nParallel execution with %d threads and %d elements\n", omp_get_num_threads(), N);
+	}
 
-	auto start = high_resolution_clock::now();
+	startReductionOMP = omp_get_wtime();
 
-#pragma omp parallel reduction(+:sum) private (i)
-	{	int id = omp_get_thread_num();
+	
+#pragma omp parallel reduction(+:sum_paral_red) shared(a, b) private (i)  num_threads(num_threads)
+	{	
+		int id = omp_get_thread_num();
 		int numtr = omp_get_num_threads();
-		for (i = id + 1; i < N; i = i + numtr)
+
+		for (i = id; i < N; i = i + numtr)
 		{
-			sum += values1[i] * values2[i];
+			sum_paral_red += a[i] * b[i];
+		}
+	}
+	endReductionOMP = omp_get_wtime();
+	printf("Reduction: %f seconds\n", endReductionOMP - startReductionOMP);
+
+	// Another approach
+	startAtomic = omp_get_wtime();
+#pragma omp parallel shared(sum_paral_atomic, a, b) private (i) num_threads(num_threads)
+	{	
+		int id = omp_get_thread_num();
+		int numtr = omp_get_num_threads();
+		for (i = id; i < N; i = i + numtr)
+		{
+#pragma omp atomic
+			sum_paral_atomic += a[i] * b[i];
 		}
 	}
 
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
+	endAtomic = omp_get_wtime();
+	printf("Atomic   : %f seconds\n", endAtomic - startAtomic);
 
-	printf("Elapsed time %lld  microseconds \n", duration.count());
-	// printf("dot product of vectors %d \n", sum);
-	return 0;
+
+	// Consequental computations
+	startConseq = omp_get_wtime();
+	for (i = 0;i < N; i++)
+	{
+		sum_seq += a[i] * b[i];
+	}
+	endConseq = omp_get_wtime();
+
+	printf("Seq      : %f seconds\n\n", endConseq - startConseq);
+
+	printf("Reduction result : %d\n", sum_paral_red);
+	printf("Atomic add result: %d\n", sum_paral_atomic);
+	printf("Sequence result  : %d\n", sum_seq);
+	printf("\n=======================================================\n");
 }
+
+int main(int argc, char** argv)
+{
+	//const int N = atoi(argv[1]);
+	const int N = 1000;
+	vector<int> a(N);
+	vector<int> b(N);
+	
+
+	auto f = []() -> int { return rand() % 10000; };
+	generate(a.begin(), a.end(), f);
+	generate(b.begin(), b.end(), f);
+
+	dotProduct(a, b, 1, N);
+	dotProduct(a, b, 2, N);
+	dotProduct(a, b, 4, N);
+	dotProduct(a, b, 8, N);
+}
+
