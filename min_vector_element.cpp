a64846c1396628d27c20c63e79e9a8add0d08957
MarieMin <marie.mingazova@mail.ru>
2020-12-14 12:42:33 +0300

    Update min_vector_element.cpp

--- min_vector_element.cpp
+++ min_vector_element.cpp
@@ -6,79 +6,97 @@
 
 using namespace std;
 
+void omp_max_el(int *arr, int N, int num_threads){
+
+        int max_val_red = 0;
+        int max_val_crit = 0;
+        int max_val_seq = 0;
+        int i;
+
+        double start_red, end_red;
+        double start_crit, end_crit;
+        double start_seq, end_seq;
+
+	
+	// deprecate dynamic threads sheduling
+        omp_set_dynamic(0);
+#pragma omp parallel num_threads(num_threads)
+        if (omp_get_thread_num() == 0)
+        {
+                printf("\n\nParallel execution with %d threads and %d elements\n", omp_get_num_threads());
+        }
+		       
+		       
+	// search with reduction directive
+        start_red = omp_get_wtime();
+#pragma omp parallel for reduction(max : max_val_red) num_threads(num_threads)
+        for (i = 0;i < N; i++)
+        {
+                if (arr[i] > max_val_red)
+                {
+                        max_val_red = arr[i];
+                }
+        }
+        end_red = omp_get_wtime();
+
+
+// search with critical directive
+        start_crit = omp_get_wtime();
+#pragma omp parallel for shared(max_val_crit) num_threads(num_threads)
+        for (i = 0;i < N; i++)
+        {
+                if (arr[i] > max_val_crit)
+                {
+#pragma omp critical
+// second check since max_val_crit could have changed during the time while the thread enters the critical section 
+// (this case can be observed with a small number of elements)
+                        if (arr[i] > max_val_crit)
+                        {
+                                max_val_crit = arr[i];
+                        }
+                }
+        }
+        end_crit = omp_get_wtime();
+
+
+
+// sequential code
+        start_seq = omp_get_wtime();
+        for (i = 0;i < N; i++)
+        {
+                if (arr[i] > max_val_seq)
+                {
+                        max_val_seq = arr[i];
+                }
+        }
+        end_seq = omp_get_wtime();
+		       
+        printf("Reduction : %f seconds\n", end_red - start_red);
+        printf("Critical  : %f seconds\n", end_crit - start_crit);
+        printf("Sequential: %f seconds\n\n", end_seq - start_seq);
+
+        printf("Reduction : %d \n", max_val_red);
+        printf("Critical  : %d \n", max_val_crit);
+        printf("Sequential: %d \n", max_val_seq);
+
+        printf("\n=================================\n");
+}
+
+
 int main(int argc, char** argv)
 {
-	const int N = atoi(argv[1]);
-	double arr[N];
-	
-	double max_val_paral = 0.0;
-	double max_val_seq = 0.0;
-	int i;
+         //const int N = atoi( argv[1]);
+         const int N = 1000;
 
-	double start1;  // parallel execution ,easure
-	double end1;
-	double start2;   // sequence execution measure
-	double end2;
+        inte *arr = (int*) malloc(N * sizeof(int));
 
+        for (int i = 0; i < N; i++)
+                arr[i] = rand() % 1000;
 
-	std::srand(1);
-	for (i = 0; i < N; i++)
-		arr[i] = rand() % 1000;
-
-	omp_set_dynamic(0);
-
-#pragma omp parallel num_threads(4)
-	/*if (omp_get_thread_num() == 0)
-	{
-		printf("\n\nParallel execution with %d threads and %d elements\n ", omp_get_num_threads(), N);
-	}*/
-
-	start1 = omp_get_wtime();
-#pragma omp parallel for reduction(max : max_val) num_threads(omp_get_max_threads())
-		for (i = 0;i < N; i++)
-		{
-			if (arr[i] > max_val_paral)
-			{
-				max_val_paral = arr[i];
-			}
-		}
-
-// OR 
-
-//#pragma omp parallel for shared(max_val_paral) num_threads(4)
-//		for (i = 0;i < N; i++)
-//		{
-//			if (arr[i] > max_val_paral)
-//			{
-//#pragma omp critical
-//				max_val_paral = arr[i];
-//			}
-//		}
-
-	end1 = omp_get_wtime();
-
-	printf("Max element with parallel execution = %f\n", max_val_paral);
-	printf("Work took %f seconds\n", end1 - start1);
-
-	start2 = omp_get_wtime();
-	for (i = 0;i < N; i++)
-	{
-		if (arr[i] > max_val_seq)
-		{
-			max_val_seq = arr[i];
-		}
-	}
-	end2 = omp_get_wtime();
-
-	printf("Max element with sequential execution = %f\n", max_val_seq);
-	printf("Work took %f seconds\n", end2 - start2);
-
-	if (max_val_paral = max_val_seq)
-	{
-		printf("Parallel and sequential sections got the same result\n");
-	}
-	else
-	{
-		printf("Error! Parallel and sequential sections got a different results\n");
-	}
+	omp_max_el(arr, N, 1);
+	omp_max_el(arr, N, 2);
+	omp_max_el(arr, N, 4);
+        omp_max_el(arr, N, 8);
+        omp_max_el(arr, N, 16);
+	omp_max_el(arr, N, 32);
 }
