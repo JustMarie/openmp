9e5429ee752652c4ab286fcdc5e5f6b766907399
MarieMin <marie.mingazova@mail.ru>
2020-12-15 12:55:13 +0300

    Update integral.cpp

--- integral.cpp
+++ integral.cpp
@@ -1,46 +1,78 @@
 #include <stdio.h>
 #include <omp.h>
 #include <cmath>
-#include <chrono>
-
-using namespace std::chrono;
 
 const unsigned int N = 100000;
-const unsigned NUM_THREADS = 4;
 
 double f(double x)
 {
 	return sin(x);
 }
 
-int main()
+void integral(int num_threads)
 {
+	double a, b;
+	double h, x;
+
+	double sum_parall, I_parall;
+	double start_parall, end_parall;
+
+	double sum_seq, I_seq;
+	double start_seq, end_seq;
 	int i;
-	double h, sum, x, I;
-	double a = 0, b = 1;
-	h = (b - a) / (double)N;
-	sum = 0.0;
 
-	auto start = high_resolution_clock::now();
+	a = 0;
+	b = 10;
+	sum_parall = 0.0;
+	sum_seq = 0.0;
+	h = (b - a) / (double)N; //step of integration
 
-	omp_set_num_threads(NUM_THREADS);
+	omp_set_dynamic(0);
 
-#pragma omp parallel reduction(+:sum) private (i,x) shared (n,h)
-	{	int id = omp_get_thread_num();
+#pragma omp parallel num_threads(num_threads)
+	if (omp_get_thread_num() == 0)
+	{
+		printf("\n\nParallel execution with %d threads and %d elements\n", omp_get_num_threads(), N);
+	}
+
+
+	start_parall = omp_get_wtime();
+#pragma omp parallel reduction(+:sum_parall) private (i, x) shared (h)
+	{	
+		int id = omp_get_thread_num();
 		int numtr = omp_get_num_threads();
 		for (i = id + 1; i < N; i = i + numtr)
 		{
 			x = h * i;
-			sum += f(x);
+			sum_parall += f(x);
 		}
 	}
-	I = h * sum;
+	I_parall = h * sum_parall;
 
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
+	end_parall = omp_get_wtime();
 
-	printf("Elapsed time %lld  microseconds \n", duration.count());
-	printf("Integral is approximately %.16f \n", I);
-	return 0;
+	start_seq = omp_get_wtime();
+	for (i = 0; i < N; i++)
+	{
+		x = h * i;
+		sum_seq += f(x);
+	}
+	I_seq = h * sum_parall;
+	end_seq = omp_get_wtime();
+
+	printf("Parallel   : %f seconds\n", end_parall - start_parall);
+	printf("Sequential : %f seconds\n", end_seq - start_seq);
+
+	printf("Parallel integral   : %f\n", I_parall);
+	printf("Sequential integral : %f\n", I_seq);
 }
 
+int main(int argc, char** argv)
+{
+	integral(1);
+	integral(2);
+	integral(4);
+	integral(8);
+	integral(16);
+	integral(32);
+}
